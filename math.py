8375f04d119a55900ff6104d6863e5e170a64873
punisher <vskupekar@gmail.com>
2022-02-01 09:28:24 +0530

    Added all function headers to math.py

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+#add implementation
 def add(x,y):
 	pass
 
+#subtract implementation
 def subtract(x,y):
 	pass
 
+#multiply implementation
 def Multiply(x,y):
 	pass
 
+#divide implementation
 def Divide(x,y):
 	pass
